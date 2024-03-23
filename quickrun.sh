9dea10b27d2b1ef1501b3911e821ba25bff237b3
NoodleGodz <baybobi4@gmail.com>
2024-03-23 17:17:36 +0700

    Create quickrun.sh

---
+++ quickrun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gnome-terminal -- java ChatClient 8080 Minh
+gnome-terminal -- java ChatClient 8080 Tuan
+gnome-terminal -- java ChatClient 8080 Dat
+gnome-terminal -- java ChatServerImpl 8080
